3ae995d2ac786e87015e3bcb936a1061df3dc367
Solvar <example@gmail.com>
2021-10-16 19:41:26 +0300

    first commit

---
+++ js/script_old.js
@@ -0,0 +1,56 @@
+"use strict";
+// не даст сделать так:
+// console.log(a);
+// console.log(b);
+// var a = 15;
+// let b = 16;
+// console.log(a);
+// console.log(b);
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+
+// // проблемы VAR
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+// console.log(result);
+// // var помойка
+// // let ES6
+
+
+// // получаем undefined (пустой холодильник)
+// let und;
+// console.log(und);
+// // получаем null:
+// console.log(something); //null (ничего нет)
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// };
+// // console.log(obj.age);
+// console.log(obj[name]);
+
+let arr = ['hh.jpg', 'orange.bpm1', {}, []];
+console.log(arr[0]);
