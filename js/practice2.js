2c7bde57e63934055090137a9c82f7fb095acf5c
Solwar <example@gmail.com>
2021-10-17 18:44:54 +0300

    Practice2 trainer commit

--- js/practice2.js
+++ js/practice2.js
@@ -13,50 +13,27 @@ let personalMovieDB = {
 };
 
 for (let index = 0; index < 2; index++) {
-    const recentFilm = prompt("Один из последних просмотренных фильмов?", "Moby"),
-    markFilm = prompt("На сколько оцените его?");
-    personalMovieDB.movies[recentFilm] = markFilm;
-    
-}
-
-let filmsCount = 0;
-
-while (filmsCount < 2) {
-    const recentFilm = prompt("Один из последних просмотренных фильмов?", "Moby"),
-    markFilm = prompt("На сколько оцените его?");
-
-    if (recentFilm.length > 50) {
-        alert("Слишком длинное название фильма!");
-    } else if (recentFilm.length === 0) {
-        alert("Заполните название фильма!");
-    } else if (recentFilm === undefined) {
-        alert("Заполните название фильма!");
-    } else if (recentFilm === " ") {
-        alert("Заполните название фильма!");
-    } else {
+    const recentFilm = prompt("Один из последних просмотренных фильмов?", ""),
+            markFilm = prompt("На сколько оцените его?");
+    // если юзер нажимает отмену то получаем  null
+    if (recentFilm != null && markFilm != null && recentFilm != "" && markFilm != "" && recentFilm.length < 50) {
         personalMovieDB.movies[recentFilm] = markFilm;
-        filmsCount++;
+        console.log("done");
+    } else {
+        console.log("error");
+        index--;
     }
-
     
 }
-// let recentFilm = prompt("Один из последних просмотренных фильмов?", "Moby"),
-//     markFilm = prompt("На сколько оцените его?");
+// ПЕРЕПИСАТЬ ЦИКЛ ЕЩЕ ДВУМЯ СПОСОБАМИ
 
-// let recentFilm2 = prompt("Один из последних просмотренных фильмов?", "Moby"),
-//     markFilm2 = prompt("На сколько оцените его?");
-
-// // во избежание багов в браузере или кириллицы в ответах пользователя к экземляру
-// //  объекта movies стоит обращаться через []
-// personalMovieDB.movies[recentFilm] = markFilm;
-// personalMovieDB.movies[recentFilm2] = markFilm2;
 
 console.log(personalMovieDB);
 if (personalMovieDB.count < 10) {
     alert("Просмотрено довольно мало фильмов!");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     alert("Вы классический зритель!");  
-} else if (personalMovieDB.count > 30){
+} else if (personalMovieDB.count >= 30){
     alert("Вы киноман!");
 } else {
     alert("Произошла ошибка!");
