2d7471f5b8250a2697edc4823cc6498aef39b22b
Solwar <example@gmail.com>
2021-11-10 16:09:34 +0300

    20lesson massives handler v1

--- js/lesson20_objects.js
+++ js/lesson20_objects.js
@@ -1,9 +1,56 @@
 "use strict";
+// not preffered method for create objects:
+// const obj = new Object(); 
 
 
 const options = {
     name: "test",
     width: 1024,
     height: 1024,
-    color
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+
+// console.log(options.name);
+
+// deleting object atributes 
+// delete options.name;
+
+// console.log(options);
+// прямых констант в JS нет!
+
+// console.log(options["colors"]["border"]); //black
+// preffered:
+// console.log(options.colors.border); // black
+
+// перебор объектов
+
+// 
+let counter = 0;
+
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        // то перебираем colors
+        for (let i in options[key]) {
+            console.log(`Св-во: ${i}, имеет значение ${options[key][i]}`);
+            // объекты со всех уровней (толькео подуровневые св-ва)
+            // counter++;
+        }
+    } else {
+        console.log(`Св-во: ${key}, имеет значение ${options[key]}`);
+        // объекты со всех уровней (верхний уровень без учета самого colors)
+        // counter++;
+    }
+    // кол-во верхнеуровневых объектов:
+    counter++; //4
 }
+// вывести кол-во объектов в массиве
+console.log(counter);
+// сейчас все значения выводятся ввиде строк, [object Object] 
+// это строковое представление объекта (jS не может вывести корректно в строку)
+
+
+// объекты нельзя итерировать через of
